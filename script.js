d8f9aebb2cff0703d79504417fd327fc42b59494
jdoug28 <joaodouglasoliveira6@gmail.com>
2024-03-17 18:23:53 -0300

    css

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
+document.getElementById("input-box").addEventListener("focus", function() {
+    var placeholderText = "Digite sua nota aqui...";
+    if (this.innerHTML.trim() === placeholderText) {
+        this.innerHTML = "";
+    }
+});
+
+document.getElementById("input-box").addEventListener("blur", function() {
+    var placeholderText = "Digite sua nota aqui...";
+    if (this.innerHTML.trim() === "") {
+        this.innerHTML = placeholderText;
+    }
+});
+
+// Definindo o texto de espaço reservado inicial
+var placeholderText = "Digite sua nota aqui...";
+document.getElementById("input-box").innerHTML = placeholderText;
+
 function criarNota() {
+    let notaContainer = document.createElement("div");
+    notaContainer.className = "divNotaCriada"
     let nota = document.createElement("p");
+    nota.className = "notaCriada";
     let botaoExcluir = document.createElement("button");
     botaoExcluir.innerHTML = "Excluir";
+    botaoExcluir.id = "btnExcluir";
 
     var contenteditable = document.querySelector('[contenteditable]');
     var text = contenteditable.innerHTML;
     
     if (text!="") {
-        nota.innerHTML = text + "<br>";
-        nota.appendChild(botaoExcluir);
-        document.body.appendChild(nota);
+        nota.innerHTML = text;
+        notaContainer.appendChild(nota);
+        notaContainer.appendChild(botaoExcluir);
+        document.body.appendChild(notaContainer);
         contenteditable.textContent = "";
     }
     else {
@@ -17,6 +40,6 @@ function criarNota() {
     }
 
     botaoExcluir.addEventListener("click", function() {
-        nota.remove();
+        notaContainer.remove();
     });
 }
